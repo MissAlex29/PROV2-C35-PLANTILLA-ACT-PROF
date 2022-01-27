b477a57e363b0eb7aff9a1fced4932a199a9c55d
MissAlex29 <92283607+MissAlex29@users.noreply.github.com>
2022-01-26 23:27:29 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var hypnoticBall, database;
+var position;
+
+function setup(){
+    createCanvas(500,500);
+    
+}
+    
+    
+function draw(){
+    background("white");
+    
+    drawSprites();
+}
+
+
+function changePosition(x,y){
+    
+}
+
